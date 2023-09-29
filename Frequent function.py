af3f7b4cb86837e070267a8426da1d70809dc5d2
arhant0605 <143576707+arhant0605@users.noreply.github.com>
2023-09-30 02:25:30 +0530

    Frequent Function.py

---
+++ Frequent function.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 29 03:14:54 2023
+
+@author: hp
+"""
+
+def most_frequent(input_string):
+    char_frequency = {}
+    for char in input_string:
+        if char.isalpha():
+            char_frequency[char] = char_frequency.get(char, 0) + 1
+    sorted_frequency = sorted(char_frequency.items(), key=lambda x: x[1], reverse=True)  
+    for char, frequency in sorted_frequency:
+        print(f"{char} = {frequency:02d}")
+
+input_string = "Misssissippi"
+most_frequent(input_string)
