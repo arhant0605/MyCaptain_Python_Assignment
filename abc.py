686f922a2daa76aff833bd381eabe6ab36d2937d
arhant0605 <143576707+arhant0605@users.noreply.github.com>
2024-06-12 19:52:19 +0530

    Add files via upload

--- abc.py
+++ abc.py
@@ -1,4 +1,16 @@
-import math 
-radius = float(input("Enter the radius of the circle: "))
-area = math.pi * radius ** 2
-print(f"The are of the circle with radius {radius} is {area:.16f} ")
+from math import pi
+def calculate_cirle_area():
+    radius = float(input("the radius of the circle : "))
+    area = pi * (radius ** 2)
+    print(f"The are of the circle with radius {radius} is {area:.16f} ")
+
+calculate_cirle_area()
+
+def get_file_extension():
+    filename = input("Input the Filename: ")    
+    file_extension = filename.split('.')[-1]
+    extension_map = {'py': 'python'}
+    extension_description = extension_map.get(file_extension, file_extension)
+    print(f"The extension of the file is : '{extension_description}'")
+
+get_file_extension()
